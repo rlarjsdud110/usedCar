47d57df4093049652489df425d970f0f3bc07c29
rlarjsdud110 <rlarjsdud110@naver.com>
2025-02-15 00:42:41 +0900

    init : initialize project

---
+++ src/test/java/com/kgy/usedCar/UsedCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgy.usedCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
