b2709ae6d52e01d60e991b345db271c6f1371c54
rlarjsdud110 <rlarjsdud110@naver.com>
2025-03-21 01:41:39 +0900

    CORS 경로 수정

--- src/main/java/com/kgy/usedCar/config/WebMvcConfig.java
+++ src/main/java/com/kgy/usedCar/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5501")
+                .allowedOrigins("https://naechaeottae.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
